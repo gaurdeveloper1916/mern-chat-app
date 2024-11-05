ec945d97813e07dc3c47691cca733a9d2b9a2c1c
unknown <amit.tyagi@leadsconnect.in>
2024-11-05 17:03:26 +0530

    add frontend

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.send("API is running 123")
+})
+app.get
+
 app.listen(5000, console.log("Server started on 5000 port"))
